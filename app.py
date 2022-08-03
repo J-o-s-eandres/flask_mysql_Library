9f09b99016398d7bad4bfa26be4b8360a671b8ab
Joseandres Montesino <joseandresmontesino@gmail.com>
2022-08-02 23:32:08 -0500

    conexion con la bd mysql

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
-from flask import render_template
-
+from flask import render_template,request , redirect
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql=MySQL()
 
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='sitio'
+mysql.init_app(app) #inicializar
 
 @app.route('/')
 def inicio():
@@ -20,7 +26,7 @@ def nosotros():
     return render_template('sitio/nosotros.html')
 
 
-@app.route("/admin")
+@app.route("/admin/")
 def admin_index():
     return render_template('admin/index.html')
 
@@ -28,5 +34,22 @@ def admin_index():
 def admin_login():
     return render_template('admin/login.html')
 
+@app.route("/admin/libros")
+def admin_libros():
+    conexion=mysql.connect()#conexion con la bd
+    print(conexion)
+    return render_template("admin/libros.html")
+
+@app.route("/admin/libros/guardar",methods=['POST'])
+def admin_libros_guardar():
+    _nombre=request.form['txtNombre']#recepcion de los archivos (va el mismo nombre que se le dio al input en el form)  
+    _url=request.form['txtURL'] #recepcion de los archivos (va el mismo nombre que se le dio al input en el form)
+    _archivo=request.files["txtImagen"] #recepcion de las imagenes (va el mismo nombre que se le dio al input en el form)
+
+    print(_nombre)
+    print(_archivo)
+    print(_url)
+    return redirect('/admin/libros')
+
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
