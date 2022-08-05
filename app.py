87ba3dcbb0e35ff66de015a69127a764fa950bd1
Joseandres Montesino <joseandresmontesino@gmail.com>
2022-08-04 22:52:25 -0500

    visualizacion de la imagen subida

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from sqlite3 import connect
+import os
 from flask import Flask
 from flask import render_template,request , redirect
 from flaskext.mysql import MySQL
-
+from datetime import datetime
+from flask import send_from_directory
 app = Flask(__name__)
 mysql=MySQL()
 
@@ -16,6 +17,10 @@ mysql.init_app(app) #inicializar
 def inicio():
     return render_template('sitio/index.html')
 
+@app.route("/img/<imagen>")
+def imagenes(imagen):
+    print(imagen)
+    return send_from_directory(os.path.join('templates/sitio/img'),imagen)
 
 @app.route("/libros")
 def libros():
@@ -41,9 +46,9 @@ def admin_libros():
     cursor = conexion.cursor()#cursor
     cursor.execute("SELECT * FROM `libros` ")
     libros = cursor.fetchall()#recuperacion de los ,obros y almacenamiento en la variable "libros"
-    conexion.commit
+    conexion.commit()
     print(libros)
-    print(conexion)
+    print(conexion) 
 
     return render_template("admin/libros.html",libros=libros)
 
@@ -53,8 +58,17 @@ def admin_libros_guardar():
     _url=request.form['txtURL'] #recepcion de los archivos (va el mismo nombre que se le dio al input en el form)
     _archivo=request.files["txtImagen"] #recepcion de las imagenes (va el mismo nombre que se le dio al input en el form)
 
+
+    tiempo = datetime.now()#hora actual
+    horaActual= tiempo.strftime('%Y%H%M%S')#formato 
+
+    if _archivo.filename!="":
+        nuevoNombre=horaActual+"_"+_archivo.filename#formato del nombre del archivo
+        _archivo.save("templates/sitio/img/"+nuevoNombre)#guardamos en la carpeta img las imagenes
+
+
     sql = "INSERT INTO `libros` (`id`, `nombre`, `imagen`, `url`) VALUES (NULL, %s, %s, %s);"
-    datos=(_nombre,_archivo.filename,_url)
+    datos=(_nombre,nuevoNombre,_url)
 
     conexion = mysql.connect()#se genera la conexion
     cursor= conexion.cursor()#se genera el cursor
@@ -66,5 +80,25 @@ def admin_libros_guardar():
     print(_url)
     return redirect('/admin/libros')
 
+@app.route("/admin/libros/borrar",methods=['POST'])
+def admin_libros_borrar():
+    _id=request.form['txtID']#recepcionamos el id
+    print(_id)
+
+    conexion=mysql.connect()#conexion con la bd
+    cursor = conexion.cursor()#cursor
+    cursor.execute("SELECT * FROM `libros` WHERE id =%s ",(_id))
+    libro = cursor.fetchall()#recuperacion de los ,obros y almacenamiento en la variable "libros"
+    conexion.commit()
+    print(libro)
+    
+
+    conexion=mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute("DELETE  FROM `libros` WHERE id =%s ",(_id))
+    conexion.commit()
+
+
+    return redirect('/admin/libros')
 if __name__=='__main__':
     app.run(debug=True) 
